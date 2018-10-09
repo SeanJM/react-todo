c3ec067d63004a2d23e0f56e1da8f6ed3e3f148b
SeanJM <seanjmacisaac@gmail.com>
2018-10-08 18:43:49 -0700

    formatted

--- packages/generate-id.ts
+++ packages/generate-id.ts
@@ -1,13 +1,16 @@
 const idLib = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0987654321";
+
 function r(a) {
   return a[Math.floor(Math.random() * (a.length - 1))];
 }
 
-export default function (length: number = 12) {
-  const id: string[] = [];
+export default function generateId(length: number = 12) {
+  let id = "";
   let i = -1;
+
   while (++i < length) {
-    id.push(r(idLib));
+    id += r(idLib);
   }
-  return id.join("");
+
+  return id;
 }
